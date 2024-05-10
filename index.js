1b88fb9d6c08ce42868528da840e2c8a4290b85c
Thapelo Boya <88501835+ThapeloBoya@users.noreply.github.com>
2024-05-10 11:29:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,31 +18,33 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-const isInvalidDate = (date) => date.toUTCString() === 'Invalid Date'
+const isInvalidDate = (date) => date.toUTCString() ==='Invalid Date'
 // your first API endpoint... 
-app.get("/api/:DATE", function (req, res) {
+app.get("/api/:date", function (req, res) {
   let date = new Date(req.params.date)
 
   if(isInvalidDate(date)){
-    date = new DataTransferItem(+req.params.date)
+    date = new Date(+req.params.date)
   }
+
   if(isInvalidDate(date)){
-   res.json({error:"Invalid Date"})
-   return;
+    res.json({error:"Invalid Date"})
+    return
   }
 
   res.json({
     unix:date.getTime(),
-    utc: date.toUTCString()
+    utc:date.toUTCString()
   });
 });
-
-app.get("/api", (req,res) =>{
+app.get("/api", (req, res) => {
   res.json({
     unix:new Date().getTime(),
-    utC: new Date().toUTCString()
+    utc:new Date().toUTCString()
   })
+
 })
+
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
